0a786b06272846cd3f76831cbd8c3e5c487c1392
hyyou <hyyou@foxmail.com>
2015-03-16 12:10:54 +0800

    auto wrap viewgroup

---
+++ src/com/maxproj/viewgrouptest/AutoWrapViewGroup.java
@@ -0,0 +1,67 @@
+package com.maxproj.viewgrouptest;
+
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class AutoWrapViewGroup extends ViewGroup {
+	public AutoWrapViewGroup(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+
+	private final static int VIEW_MARGIN = 10;
+
+	public AutoWrapViewGroup(Context context) {
+		super(context);
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+		for (int index = 0; index < getChildCount(); index++) {
+			final View child = getChildAt(index);
+			// measure
+			child.measure(MeasureSpec.UNSPECIFIED, MeasureSpec.UNSPECIFIED);
+		}
+
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+	@Override
+	protected void onLayout(boolean arg0, int arg1, int arg2, int arg3, int arg4) {
+		Log.d("", "onLayout parants： changed = " + arg0 + " left = " + arg1
+				+ " top = " + arg2 + " right = " + arg3 + " botom = " + arg4);
+
+		int parants_width = arg3 - arg1;//ViewGroup宽度
+		
+		final int count = getChildCount();
+		int rightMove = 0;
+		int bottomMove = this.getChildAt(0).getMeasuredHeight();
+		
+		for (int i = 0; i < count; i++) {
+
+			View child = this.getChildAt(i);
+			int width = child.getMeasuredWidth();
+			int height = child.getMeasuredHeight();
+			
+			
+			rightMove += width + VIEW_MARGIN;
+
+			if (rightMove > parants_width) {//换行
+				rightMove = width + VIEW_MARGIN;
+				bottomMove += height + VIEW_MARGIN;
+			}else{
+				
+			}
+			child.layout(rightMove - width, bottomMove - height, rightMove, bottomMove);
+			Log.d("","onLayout params： "+(rightMove)+" "+(bottomMove) +" "+ width+" "+height);
+			Log.d("","onLayout child： "+(rightMove - width)+" "+(bottomMove - height) +" "+ rightMove+" "+bottomMove);
+		}
+//		invalidate();
+	}
+
+}
---
+++ src/com/maxproj/viewgrouptest/ViewGroupTestActivity.java
@@ -0,0 +1,71 @@
+package com.maxproj.viewgrouptest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class ViewGroupTestActivity extends Activity {
+
+	AutoWrapViewGroup viewgroup_grid;
+	AutoWrapViewGroup viewgroup_grid2;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_view_group_test);
+		
+		viewgroup_grid = (AutoWrapViewGroup)findViewById(R.id.viewgroup_grid);
+		viewgroup_grid2 = (AutoWrapViewGroup)findViewById(R.id.viewgroup_grid2);
+		
+		addTextViewToAddedViewGroup(viewgroup_grid, "1----------");
+		addTextViewToAddedViewGroup(viewgroup_grid, "2----");
+		addTextViewToAddedViewGroup(viewgroup_grid, "3------");
+		addTextViewToAddedViewGroup(viewgroup_grid, "4===");
+		addTextViewToAddedViewGroup(viewgroup_grid, "5======");
+		addTextViewToAddedViewGroup(viewgroup_grid, "6~~~");
+		addTextViewToAddedViewGroup(viewgroup_grid, "7++++++");
+		addTextViewToAddedViewGroup(viewgroup_grid, "8**** ");
+		addTextViewToAddedViewGroup(viewgroup_grid, "9= ");
+		addTextViewToAddedViewGroup(viewgroup_grid, "10_____ ");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "11^^^^^^^^^^^^^^^^^");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "1----------");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "2----");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "3------");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "4===");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "5======");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "6~~~");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "7++++++");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "8**** ");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "9= ");
+		addTextViewToAddedViewGroup(viewgroup_grid2, "10_____ ");
+		
+	}
+	
+	public void addTextViewToAddedViewGroup(AutoWrapViewGroup awvg, String input_string) {
+		TextView tv = new TextView(this);
+		tv.setText(input_string);
+		awvg.addView(tv);
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.view_group_test, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+		if (id == R.id.action_settings) {
+			return true;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+}
